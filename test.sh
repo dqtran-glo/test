b0037cd01d0501f5897a4132a0def350599f83e0
dqtran <dqtran@glo.com>
2026-04-18 09:34:37 +0700

    test6

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 set -a
 source .env
+
 set +a
+echo "PAT length: ${#PAT}"
+
 
 git push "https://x-access-token:${PAT}@github.com/dqtran-glo/test2.git" \
   HEAD:refs/heads/ci-write-test-$(date +%s)
